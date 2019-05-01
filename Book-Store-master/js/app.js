5ed6480c1480bb422daa92fecb3611ad1367bda5
Tapas Mondal <mondaltapas02@gmail.com>
2019-05-01 13:18:36 +0530

    Book store

---
+++ Book-Store-master/js/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function()    {
+
+	$('.multi-item-carousel').carousel({
+		interval: false
+	});
+
+	$('.multi-item-carousel .item').each(function(){
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		if (next.next().length>0) {
+			next.next().children(':first-child').clone().appendTo($(this));
+		} else {
+			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+		}
+	});
+
+	$('#slider').rotateSlider({
+
+	});
+
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
+
+	$('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+
+});
